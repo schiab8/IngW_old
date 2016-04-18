470447975e7f42eb4598ff5d5f006c88f7f7eab3
Diego Serruya <Diego Serruya>
2016-04-18 17:36:27 -0300

    Renombrado el proyecto

--- takuar/takuar/settings.py
+++ takuar/takuar/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Entrega2.urls'
+ROOT_URLCONF = 'takuar.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Entrega2.wsgi.application'
+WSGI_APPLICATION = 'takuar.wsgi.application'
 
 
 # Database
