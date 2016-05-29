b996cf7d50965bbd975cf55d918824714a3fa3a8
schiab8 <santiagochiabotto@gmail.com>
2016-05-29 04:45:49 -0300

    Agregado link a perfil en busqueda de usuario

--- takuar/forum/models.py
+++ takuar/forum/models.py
@@ -4,8 +4,6 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class Forum(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
