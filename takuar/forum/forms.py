300495af471bcf07234d8ef238d5e9aab7fddbc7
Santiago Chiabotto <schiab8@blExo.sntgo>
2016-06-17 18:28:22 -0300

    Labels en formularios de foros. Linebreaks.

--- takuar/forum/forms.py
+++ takuar/forum/forms.py
@@ -5,10 +5,20 @@ class FormThread(forms.ModelForm):
     class Meta:
         model=Thread
         exclude=['submit_date']
-        widgets = {'forum':forms.HiddenInput(),'author':forms.HiddenInput(),}
+        widgets = {
+                'forum':forms.HiddenInput(),
+                'author':forms.HiddenInput(),
+                }
+        labels = {
+                'name':('Titulo del tema:'),
+                'content':('Contenido'),
+                }
 
 class FormReply(forms.ModelForm):
     class Meta:
         model=Reply
         exclude=['submit_date']
         widgets = { 'author': forms.HiddenInput(), 'thread': forms.HiddenInput(),}
+        labels = {
+                'message':('Respuesta'),
+                }
